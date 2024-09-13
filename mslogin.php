1f832ceebfdd834308445c6a72b65ec463a6d917
RavendraDotJava <124779917+RavendraDotJava@users.noreply.github.com>
2024-09-13 11:20:21 +0530

    Update mslogin.php

--- mslogin.php
+++ mslogin.php
@@ -1,4 +1,5 @@
 <?php
+// require adnanhussainturki/microsoft-api-php this library using composer before using
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
