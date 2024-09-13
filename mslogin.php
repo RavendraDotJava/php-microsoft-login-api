795ab327036aae898e120a6e4cffa472a22d7f44
RavendraDotJava <124779917+RavendraDotJava@users.noreply.github.com>
2024-09-13 11:14:04 +0530

    Update mslogin.php

--- mslogin.php
+++ mslogin.php
@@ -8,9 +8,9 @@ require "vendor/autoload.php";
 use myPHPnotes\Microsoft\Auth;
 
 $tenant = "common";
-$client_id = "1e6d4bd0-520a-4e7c-ae1d-5f40f8abd0e6";
-$client_secret = "CJA8Q~0NQaSlVLu6bUBZR8vxdjd7kxyuRt5p3aG-";
-$callback = "https://resolvegroup.in/projects/rust-consolecommunity/callback.php";
+$client_id = "Client Id";
+$client_secret = "Client secret";
+$callback = "Your Callback Url";
 $scopes = [
     'User.Read'
 ];
