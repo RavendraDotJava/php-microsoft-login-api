4161260177cac683de0991b07e1a1bfd15692a06
RavendraDotJava <124779917+RavendraDotJava@users.noreply.github.com>
2024-09-13 11:17:00 +0530

    Update callback.php

--- callback.php
+++ callback.php
@@ -4,9 +4,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $tenant = "common";
-$client_id = "1e6d4bd0-520a-4e7c-ae1d-5f40f8abd0e6";
-$client_secret = "CJA8Q~0NQaSlVLu6bUBZR8vxdjd7kxyuRt5p3aG-";//client secret value 
-$callback = "https://resolvegroup.in/projects/rust-consolecommunity/callback.php";
+$client_id = "your client_id";
+$client_secret = "";//client secret value 
+$callback = "callback URl";
 $scopes = [
     'User.Read'
 ];
@@ -30,6 +30,7 @@ $user = (new User); // User get pulled only if access token was generated for sc
 //print_r($user->data);
 
 //Checking if the user with email already registered.
+//do your db step to store information about user after authentication.
 $checkmail=$db->getRow("select * from registers where emailid='{$user->data->getMail()}' ");
 //echo json_encode($db->getLastQuery());exit;
              if(isset($checkmail['emailid']) && $checkmail['emailid']!=''){	
